6b602f115a0b1937e25312f80d8006247e7cb981
José Matheus <josematheuslima25@gmail.com>
2025-11-26 16:44:57 -0300

    select password to false

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,7 +2,7 @@ import mongoose, { Schema, models, model } from "mongoose";
 
 const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, select: false },
   name: { type: String, required: true },
 }, { timestamps: true });
 
