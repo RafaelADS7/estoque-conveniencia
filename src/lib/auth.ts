199102bf4a585969b848c594b5f10d8598dafe60
José Matheus <josematheuslima25@gmail.com>
2025-11-26 16:59:02 -0300

    return password to auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -16,7 +16,8 @@ export const authOptions: NextAuthOptions = {
         await dbConnect();
         
         // 1. Buscar usuário
-        const user = await User.findOne({ email: credentials?.email });
+        // O .select("+password") força o retorno do campo oculto
+        const user = await User.findOne({ email: credentials?.email }).select("+password");
         if (!user) throw new Error("Usuário não encontrado");
 
         // 2. Verificar senha
